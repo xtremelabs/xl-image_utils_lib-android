3f570cae069bf9bf9df831b67531c272f396212e
Jamie Halpern <james.halpern@xtremelabs.com>
2013-01-10 17:25:47 -0500

    Jamie H - Added another copyright notice. Made the DatabaseCache default visibility.

--- xl_image_utils_lib/src/com/xtremelabs/imageutils/DatabaseCache.java
+++ xl_image_utils_lib/src/com/xtremelabs/imageutils/DatabaseCache.java
@@ -18,7 +18,7 @@ package com.xtremelabs.imageutils;
 
 import java.util.HashMap;
 
-public class DatabaseCache {
+class DatabaseCache {
 	private final HashMap<String, Long> mUrlToLastUpdatedTime = new HashMap<String, Long>();
 	private final HashedQueue<String> hashedUrlQueue = new HashedQueue<String>();
 
