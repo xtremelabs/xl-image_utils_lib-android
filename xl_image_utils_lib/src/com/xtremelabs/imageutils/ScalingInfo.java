804693113c2036148d60c8e7e571baab54ab1fff
Jamie Halpern <james.halpern@xtremelabs.com>
2013-08-12 10:13:58 -0700

    Jamie H - Removed the equals method in the ScalingInfo object as it is not used, and never worked.

--- xl_image_utils_lib/src/com/xtremelabs/imageutils/ScalingInfo.java
+++ xl_image_utils_lib/src/com/xtremelabs/imageutils/ScalingInfo.java
@@ -20,23 +20,4 @@ class ScalingInfo {
 	public Integer sampleSize = null;
 	public Integer width;
 	public Integer height;
-
-	@Override
-	public boolean equals(Object o) {
-		if (o == null) {
-			return false;
-		}
-
-		if (!(o instanceof ScalingInfo)) {
-			return false;
-		}
-
-		ScalingInfo info = (ScalingInfo) o;
-
-		if (info.sampleSize != sampleSize || info.width != width || info.height != height) {
-			return false;
-		}
-
-		return true;
-	}
 }
